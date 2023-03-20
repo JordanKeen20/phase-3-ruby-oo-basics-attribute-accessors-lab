fbdbcd38b78e9c274527094c9d0ed29fc7d1f32c
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2023-03-19 17:00:31 -0700

    complete

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
+require "pry"
 ## code your solution here. 
+class Cat
+
+        attr_accessor :name
+        
+
+    def initialized(name)
+        @name= name
+    end
+    
+   def meow 
+        puts "meow!"
+    end
+
+end
